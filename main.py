4e4c9719260ac6e03d503d9645f0b4ddf45acab4
dlgrv <68144056+dlgrv@users.noreply.github.com>
2021-03-10 15:19:36 +0300

    V0.2 (not work)

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from decimal import *
 
 root = Tk()
 root.title('Calculator by dlgrv')
+root["bg"] = '#000000'
+root.resizable(width=False, height=False)
+
 
 # МАССИВ С КНОПКАМИ
 buttons_list = [
@@ -11,22 +14,30 @@ buttons_list = [
     ['1', '2', '3',  '*'],
     ["0", ".", "=",  '/']
 ]
-counter_x = 0
-counter_y = 0
-size_btn_x = 30
-size_btn_y = 30
-for btn_y in buttons_list:
-    for btn_x_y in btn_y:
-        btn = Button(text=btn_x_y,
-                     background='#000000',
-                     foreground='#ffffff',
-                     padx=str(size_btn_x),
-                     pady=str(size_btn_y),
-                     font=('courier', 16))
-        btn.grid(row=counter_y, column=counter_x)
-        counter_x += 1
-    counter_x = 0
-    counter_y += 1
+
+def output_window():
+    Label(root, text='', font='Courier 12', fg='#ffffff', bg='#000000').grid(row=0, column=0, columnspan=5)
+    Label(root, text='Hello word', font='Courier 30', fg='#ffffff', bg='#000000').grid(row=1, column=0, columnspan=5)
+    Label(root, text='', font='Courier 12', fg='#ffffff', bg='#000000').grid(row=2, column=0, columnspan=5)
+
+
+def draw_buttons():
+    counter_x = 1
+    counter_y = 3
+    size_btn_x = 30
+    size_btn_y = 30
+    for btn_y in buttons_list:
+        for btn_x_y in btn_y:
+            btn = Button(text=btn_x_y,
+                         background='#000000',
+                         foreground='#ffffff',
+                         padx=str(size_btn_x),
+                         pady=str(size_btn_y),
+                         font='Courier 20')
+            btn.grid(row=counter_y, column=counter_x)
+            counter_x += 1
+        counter_x = 1
+        counter_y += 1
 
 # ВВЕДЕНЫЕ ЧИСЛА И ОПЕРАЦИИ
 stack = []
@@ -50,5 +61,6 @@ def calculate():
         result = operand_1 / operand_2
 
     # ППРИВОДИМ ЧИСЛО В КРАСИВЫЙ ВИД (ЕСЛИ ЦЕЛОЕ, ТО
-
+output_window()
+draw_buttons()
 root.mainloop()
